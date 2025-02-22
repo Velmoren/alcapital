9129c3673ca57cf8e07fdf05dd9ec22919deee2b
Velmoren <velmoren@yahoo.com>
2025-02-22 14:26:14 +0300

    add mission page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        mission: resolve(__dirname, 'mission.html'),
       },
     },
   },
