0cbed8c2c9cbe1dc269d1d97c0fc06374714d347
Velmoren <velmoren@yahoo.com>
2025-02-27 19:15:08 +0300

    fix bugs

--- src/scripts/modules/marquee.js
+++ src/scripts/modules/marquee.js
@@ -2,16 +2,20 @@ import Splide from '@splidejs/splide';
 import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
 export const marqueeModule = () => {
-  new Splide('.marquee-splide', {
-    type: 'loop',
-    drag: 'free',
-    focus: 'center',
-    autoWidth: true,
-    gap: 20,
-    arrows: false,
-    pagination: false,
-    autoScroll: {
-      speed: 2,
-    },
-  }).mount({AutoScroll });
+  const marquee = document.querySelector('.marquee-splide')
+
+  if (marquee) {
+    new Splide('.marquee-splide', {
+      type: 'loop',
+      drag: 'free',
+      focus: 'center',
+      autoWidth: true,
+      gap: 20,
+      arrows: false,
+      pagination: false,
+      autoScroll: {
+        speed: 2,
+      },
+    }).mount({ AutoScroll });
+  }
 }
